e3007f73ca20d03b3dd03d568b4e0a63ec7736ca
PeimanHaghighat <peiman.haghighat@yahoo.ca>
2024-10-24 15:24:16 -0600

    Add another test for checking the message after clicking the Start Battle button.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { act } from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { fireEvent } from "@testing-library/react";
 
 test("displays Pokemon Battle heading", () => {
   render(<App />);
   const headingElement = screen.getByText(/Pokemon Battle/i);
   expect(headingElement).toBeInTheDocument();
 });
+
+test("displays a message after clicking the Start Battle button", async () => {
+  act(async () => {
+    render(<App />);
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    const startBattleButton = screen.getByText(/Start Battle/i);
+    expect(startBattleButton).toBeInTheDocument();
+
+    fireEvent.click(startBattleButton);
+
+    const resultMessage = await screen.findByText(/beat|same power/i);
+    expect(resultMessage).toBeInTheDocument();
+  });
+}, 10000);
